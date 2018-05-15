0f9510d970789ddc3357bfb8e7f6aaeaafb7dc2b
jmcomber <jmcomber1@gmail.com>
2018-05-15 18:30:00 -0400

    moving forward: object func 1st stage done: currently at 1st stage constrs

--- structer.jl
+++ structer.jl
@@ -28,6 +28,55 @@ for i in CORE[2]
 end
 
 FIRST_STG_COLS = unique(FIRST_STG_COLS)
-# @variable(m, x[i = FIRST_STG_COLS] ¿>= 0?)
+
+# HERE I'M NOT YET CONSIDERING BOUNDS: PLEASE DO NOT FORGET
+@variable(m, x[i = FIRST_STG_COLS])
+
+# OBTENER FUNCIÓN OBJETIVO: PARA CADA LÍNEA DE COLUMNS VER SI SALE OBJECTRW EN i[2] o i[4] (revisar length antes) (1er caso meter i[3], en el 2do i[5])
+# Las que no aparezcan deben ser un 0, y de ahí crear el producto punto entre x y estos valores, y minimizar
+
+FIRST_STG_OBJECT = []
+for i in CORE[2]
+    if i[2] == "OBJECTRW"
+        push!(FIRST_STG_OBJECT, [i[1], i[3]])
+    elseif length(i) > 3 && i[4] == "OBJECTRW"
+        push!(FIRST_STG_OBJECT, [i[1], i[3]])
+    end
+end
+
+FIRST_STG_OBJECT = [[get(i[1]), get(i[2])] for i in FIRST_STG_OBJECT]
+# println(FIRST_STG_OBJECT)
+
+
+
+AUX = [i[1] for i in FIRST_STG_OBJECT]
+for var in FIRST_STG_COLS
+    if !(var in AUX)
+        push!(FIRST_STG_OBJECT, [var, 0.0])
+    end
+end
+
+AUX = Dict(i[1] => i[2] for i in FIRST_STG_OBJECT)
+
+@objective(m, :Min, sum(AUX[i] * x[i] for i=FIRST_STG_COLS));
+
+
+FIRST_STG_ROWS = []
+for i in CORE[1]
+    if i[1] == STG2_R
+        break
+    end
+    push!(FIRST_STG_ROWS, i)
+end
+
+for row in FIRST_STG_ROWS
+    comp, name = row
+    # add constraint indicated by this row: we need RHS and all columns which include this row first
+
+
+
+end
+
+
 
 
